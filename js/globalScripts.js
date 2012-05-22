de0c65c08bc5f9b04e0522b0d2d38d064929e594
Michael Cooper <developer@kandan.com.au>
2012-05-22 18:15:21 +1000

    try current Location

--- js/globalScripts.js
+++ js/globalScripts.js
@@ -1,5 +1,5 @@
 ﻿// JavaScript Document
-var main ="http://maps.google.com.au/maps?saddr=Paradise,+Otago,+New+Zealand&daddr=Mount+Creighton,+Otago,+New+Zealand+to:Queenstown,+Otago,+New+Zealand+to:Arrow+Junction,+Otago,+New+Zealand+to:Cardrona,+Otago,+New+Zealand+to:Wanaka,+Otago,+New+Zealand+to:Cromwell,+Otago,+New+Zealand+to:Tarras,+Otago,+New+Zealand+to:Wanaka,+Otago,+New+Zealand&hl=en&ll=-44.887012,168.906555&spn=0.773523,1.436462&sll=-44.887012,168.901062&sspn=0.773523,1.436462&geocode=Fb5NU_0dOowJCiG-yb85cbDP2Q%3BFQ1bUf0d3ioKCikHixXQ_wDVqTGgXnmEhu8ABQ%3BFQbhUP0ddJYNCilf3qjX8R3VqTEApnmEhu8ABQ%3BFTiwUf0dIG4QCikVrrKY3SPVqTFgXHmEhu8ABQ%3BFQEnU_0dYsoSCimdAjt4WD_VqTHQzXmEhu8ABQ%3BFcn6Vf0dor0UCilvLey5HUbVqTHgwXmEhu8ABQ%3BFfDTUP0dGtAVCik7XtJUFCvVqTFQaHmEhu8ABQ%3BFfrYU_0d0AgZCinFMElUq8UqqDHwzXmEhu8ABQ%3BFcn6Vf0dor0UCilvLey5HUbVqTHgwXmEhu8ABQ&mra=ls&t=m&z=10"
+var main ="http://maps.google.com.au/maps?saddr=CurrentLocation&hl=en&mra=mdsmb&disamb=0&t=m&z=10"
 
 
 
