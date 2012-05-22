80c25e1f098dbdc6c2b1aad34972cd9d484cd046
Michael Cooper <developer@kandan.com.au>
2012-05-22 17:30:32 +1000

    open new window

---
+++ js/globalScripts.js
@@ -0,0 +1,11 @@
+﻿// JavaScript Document
+var main ="http://maps.google.com.au/maps?saddr=Paradise,+Otago,+New+Zealand&daddr=Glenorchy,+Otago,+New+Zealand+to:Mount+Creighton,+Otago,+New+Zealand+to:Queenstown,+Otago,+New+Zealand+to:Arrow+Junction,+Otago,+New+Zealand+to:Cardrona,+Otago,+New+Zealand+to:Wanaka,+Otago,+New+Zealand+to:Cromwell,+Otago,+New+Zealand+to:Tarras,+Otago,+New+Zealand+to:Wanaka,+Otago,+New+Zealand&hl=en&ll=-44.887012,168.901062&spn=0.761847,1.779785&sll=-44.86171,168.910675&sspn=0.762182,1.779785&geocode=Fb5NU_0dOowJCiG-yb85cbDP2Q%3BFYqiU_0dZWYJCil_mbBzH6TVqTEAcXmEhu8ABQ%3BFQ1bUf0d3ioKCikHixXQ_wDVqTGgXnmEhu8ABQ%3BFQbhUP0ddJYNCilf3qjX8R3VqTEApnmEhu8ABQ%3BFTiwUf0dIG4QCikVrrKY3SPVqTFgXHmEhu8ABQ%3BFQEnU_0dYsoSCimdAjt4WD_VqTHQzXmEhu8ABQ%3BFcn6Vf0dor0UCilvLey5HUbVqTHgwXmEhu8ABQ%3BFfDTUP0dGtAVCik7XtJUFCvVqTFQaHmEhu8ABQ%3BFfrYU_0d0AgZCinFMElUq8UqqDHwzXmEhu8ABQ%3BFcn6Vf0dor0UCilvLey5HUbVqTHgwXmEhu8ABQ&oq=Wana&mra=ls&t=m&z=10"
+
+
+
+
+function loadMap(url){
+	window.open (url,"MapsWindow");
+	
+	
+	}
