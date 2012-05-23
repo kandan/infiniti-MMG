542d4054e43369858b99b37db5258899ed7ed289
Michael Cooper <developer@kandan.com.au>
2012-05-23 14:29:57 +1000

    photo trial

--- js/globalScripts.js
+++ js/globalScripts.js
@@ -1,48 +1,12 @@
 ﻿// JavaScript Document
 
-	var pictureSource;   // picture source
-    var destinationType; // sets the format of returned value 
 
-    // Wait for Cordova to connect with the device
-    //
-    document.addEventListener("deviceready",onDeviceReady,false);
 
-    // Cordova is ready to be used!
-    //
-    function onDeviceReady() {
-        pictureSource=navigator.camera.PictureSourceType;
-        destinationType=navigator.camera.DestinationType;
-    }
+function loadMap(url){
+	window.open (url,"MapsWindow");
+	}
 
-    // Called when a photo is successfully retrieved
-    //
-	function getImage() {
-		
-	    navigator.device.capture.captureImage(onSuccess, onFail,
 	
-									{ 
-									  limit: 1
-									}
-	);
-	}
-
-
-
-	function onFail(error) {
-	    alert("Fail when getting image. Code = " + error.code);
-	}
-
-
-    
-
-    // Called when a photo is successfully retrieved
-    //
-    function onSuccess(imageURI) {
-      // Uncomment to view the image file URI 
-	alert('Your Image has been stored in the image library.');
-      // Get image handle
-      //
-    }
 
     
     
@@ -60,9 +24,3 @@ var main ="http://maps.google.com.au/maps?saddr=Current%20Location&hl=en&mra=mds
 
 
 
-
-function loadMap(url){
-	window.open (url,"MapsWindow");
-	
-	
-	}
